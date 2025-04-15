0730ceb6fd36d6a6ac2d69e4402b1699a8ba8c9f
Shikha <shimalho1@publicisgroupe.net>
2025-04-15 09:15:25 +0530

    Wrkflow A

---
+++ test/E2E_Scenarios/test_E2E_s3ToAzure.py
@@ -0,0 +1,3 @@
+class TestS3ToAzure:
+    def test_S3ToAzure(self):
+        print("S3ToAzure")
---
+++ test/test_RegisterDataset.py
@@ -0,0 +1,9 @@
+class TestRegisterDataset:
+    def register_stepA(self):
+        print("Step A done")
+
+    def register_stepB(self):
+        print("Step B done")
+
+
+
